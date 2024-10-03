18d2fca754567c6279c94258774474a9f325eb32
shubhambhatia2103 <shubhambhatia2103@gmail.com>
2024-10-03 21:38:03 +0530

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
       <div className='flex items-center justify-between mt-4 mx-3'>
         <div className='flex items-center justify-center gap-3 lg:gap-5'>
           <a href="#"><h1 className="text-2xl font-bold text-gray-900 lg:text-3xl">Shubham <span className='text-blue-500 font-cursive'>Bhatia</span></h1></a>
-          <a href="https://drive.google.com/file/d/1cuJ1HAqtWZJ9dy8rpzC6lUSB48InuNa7/view" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/1V0M9R1j_rNhdKrSEPizKf9BW-aRsBfyG/view" target="_blank" rel="noopener noreferrer">
             <button type="button" className='bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-xs font-semibold px-2 py-1 rounded-full lg:text-sm lg:px-3'>
               Resume
             </button>
